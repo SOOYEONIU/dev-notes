772171714d2056c974c9093121d5a95f02aaee3b
kimsooyeon <rlatndus235@gmail.com>
2024-04-17 08:56:27 +0900

    Feat : todoList
    추가 삭제 기능까지 완료

---
+++ TodoList/app.js
@@ -0,0 +1,26 @@
+const plusBtnAction = () => { 
+    const inputValue = document.querySelector('#inputValue');
+
+    const newLi = document.createElement('li');
+    const checkBtn = document.createElement('button');
+    const newDiv = document.createElement('div');
+    const cancelBtn = document.createElement('button');
+    cancelBtn.innerText = '삭제';
+
+    newLi.appendChild(checkBtn);
+    newLi.appendChild(newDiv);
+    newLi.appendChild(cancelBtn);
+    console.log(newLi)
+    newDiv.textContent = inputValue.value;
+
+    const todoUl = document.querySelector('#todoList');
+    todoUl.appendChild(newLi);
+
+    checkBtn.addEventListener('click', () => { 
+        newLi.classList.toggle('complete');
+    })
+
+    cancelBtn.addEventListener('click', () => { 
+        newLi.remove();
+    })
+}//plusBtnAction
