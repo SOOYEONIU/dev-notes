228de980bc58e916db6eca8bc5db8ca839f1ea6c
kimsooyeon <rlatndus235@gmail.com>
2024-04-17 09:35:13 +0900

    Styled : Add CSS

--- TodoList/app.js
+++ TodoList/app.js
@@ -1,26 +1,30 @@
 const plusBtnAction = () => { 
+    
     const inputValue = document.querySelector('#inputValue');
 
-    const newLi = document.createElement('li');
-    const checkBtn = document.createElement('button');
-    const newDiv = document.createElement('div');
-    const cancelBtn = document.createElement('button');
-    cancelBtn.innerText = '삭제';
+    if (inputValue.value !== '') {
+        const newLi = document.createElement('li');
+        const checkBtn = document.createElement('button');
+        const newDiv = document.createElement('div');
+        const cancelBtn = document.createElement('button');
+        cancelBtn.innerText = '삭제';
 
-    newLi.appendChild(checkBtn);
-    newLi.appendChild(newDiv);
-    newLi.appendChild(cancelBtn);
-    console.log(newLi)
-    newDiv.textContent = inputValue.value;
 
-    const todoUl = document.querySelector('#todoList');
-    todoUl.appendChild(newLi);
+        newLi.appendChild(checkBtn);
+        newLi.appendChild(newDiv);
+        newLi.appendChild(cancelBtn);
+        console.log(newLi)
+        newDiv.textContent = inputValue.value;
 
-    checkBtn.addEventListener('click', () => { 
-        newLi.classList.toggle('complete');
-    })
+        const todoUl = document.querySelector('#todoList');
+        todoUl.appendChild(newLi);
 
-    cancelBtn.addEventListener('click', () => { 
-        newLi.remove();
-    })
+        checkBtn.addEventListener('click', () => {
+            newLi.classList.toggle('complete');
+        })
+
+        cancelBtn.addEventListener('click', () => {
+            newLi.remove();
+        })
+    }
 }//plusBtnAction
